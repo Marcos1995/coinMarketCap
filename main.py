0a6d7cbeb0daf851f76bdca7d872f6de23b6d406
Marcos Perez <marcospc1995@gmail.com>
2021-08-05 09:28:34 +0200

    buy and sell on pancakeswap

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import triggerClass as tC
 # -------------------------------------------------------------------------------------------
 
 e = tC.cmc(
-    gainTrigger=100,  # %
-    loseTrigger=-99,  # %
+    sellTrigger=100,  # %
+    buyTrigger=-99,  # %
     sendNotifications=True,
+    isSimulation=False,
     moveHistoryCsv="moveHistory.csv",
     delay=30
 )
 
 # e.core()
-e.buyToken()
+e.sellToken()

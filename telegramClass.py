ae36a242bcb74514031a02adb25b90b7e8c6842b
Marcos Perez <marcospc1995@gmail.com>
2021-10-13 19:15:00 +0200

    improve contracts obtention from telegram groups

--- telegramClass.py
+++ telegramClass.py
@@ -7,6 +7,8 @@ import bcolors
 import asyncio
 import asyncstdlib
 import time
+import pandas as pd
+import os
 
 # --------------------------------------------------------------------------------
 
@@ -24,7 +26,7 @@ def findContractInTelegramMessages(message, pattern):
         return coincidences
 
     # Delete all previous characters before "0X"
-    coincidences = [re.sub(r'^.*?0X', '0X', coincidence) for coincidence in coincidences]
+    coincidences = [re.sub(r'^.*?0X', "0X", coincidence) for coincidence in coincidences]
 
     # Remove duplicates
     coincidences = list(dict.fromkeys(coincidences))
@@ -49,7 +51,16 @@ def findContractInTelegramMessages(message, pattern):
 
 class telegram:
 
-    def __init__(self):
+    def __init__(self, upcomingCryptosCsv):
+
+        # Csv file where we save the upcoming cryptos Telegram group name and contract
+        self.upcomingCryptosCsv = upcomingCryptosCsv
+
+        # Do we need to write headers in the .csv file?
+        if os.path.exists(self.upcomingCryptosCsv):
+            self.writeUpcomingCryptosCsvHeaders = False
+        else:
+            self.writeUpcomingCryptosCsvHeaders = True
 
         self.telegramBaseUrl = "https://t.me/"
         self.telegramBaseGroupName = "fairlaunchnewstoday"
@@ -64,11 +75,20 @@ class telegram:
         self.api_hash2 = "00790f70543b5a7f94547510d5ebbe84"
         self.phone_number2 = "+34635453357"
 
+        # ----------------
+
+        self.upcomingCryptosDesc = "upcomingCryptos"
+        self.contractsDesc = "contracts"
+
+        # Let's start the code
         self.core()
+
+    # --------------------------------------------------------------------------------
 
     @asyncstdlib.lru_cache()
     async def get_entity(client, group):
         return await client.get_entity(group)
+
 
     def getUpcomingCryptos(self, groupNamesList=None):
 
@@ -79,11 +99,11 @@ class telegram:
             upcomingCryptoContracts = {}
 
         # Connect to telegram and get messages from there
-        with TelegramClient('Marcos123', self.api_id, self.api_hash) as client:
+        with TelegramClient('Marcos', self.api_id, self.api_hash) as client:
 
             if not client.is_user_authorized():
-                client.send_code_request(self.phone_number2)
-                me = client.sign_in(self.phone_number2, input('Enter code: '))
+                client.send_code_request(self.phone_number)
+                #me = client.sign_in(self.phone_number, input('Enter code: '))
 
             for group in groups:
 
@@ -132,19 +152,23 @@ class telegram:
                     # We are trying to get all crypto groups
                     if groupNamesList is None:
 
+                        upcomingCryptos = []
+
                         for i, message in enumerate(posts.messages):
                             m = str(message.message)
 
                             if len(m) > 1000:
                                 upcomingCryptos = self.getAllTelegramGroupsByMessage(message=m)
                                 return upcomingCryptos
+                        
+                        return upcomingCryptos
 
                     else: # Let's find all the contracts
 
                         # Convert all messages to strings
                         allMessages = [str(m) for m in posts.messages]
 
-                        # Lists items to string
+                        # Lists items to a single string
                         allMessagesJoined = " ".join(allMessages)
 
                         # Find all possible contracts
@@ -158,11 +182,28 @@ class telegram:
                 except Exception as e:
                     printInfo(e, bcolors.ERRMSG)
 
-
+        # Print all valid contracts (if we only found 1 distinct contract in a group) of the Telegram groups
         for g, contract in upcomingCryptoContracts.items():
             printInfo(f"{g} = {contract}", bcolors.OKMSG)
 
+        # Dict to pd.DataFrame() --> list(df.items()) is required for Python 3.x
+        df = pd.DataFrame(list(upcomingCryptoContracts.items()), columns=[self.upcomingCryptosDesc, self.contractsDesc])
+
+        # Check
+        print(df)
+
+        #tempDf.to_csv(self.tradingHistoryCsv, index=False, columns=list(tempDf), mode="a", header=self.writeTradingHistoryHeaders)
+        df.to_csv(self.upcomingCryptosCsv, index=False, columns=list(df), mode="a", header=self.writeUpcomingCryptosCsvHeaders)
+
+
+    # Scrap all Telegram groups we found in the message
     def getAllTelegramGroupsByMessage(self, message):
+
+        #printInfo(message)
+
+        message = message.split("𝗢𝗡𝗚𝗢𝗜𝗡𝗚 𝗪𝗛𝗜𝗧𝗘𝗟𝗜𝗦𝗧:")[0]
+
+        #printInfo(message)
 
         telegramGroups = []
 
@@ -170,11 +211,23 @@ class telegram:
             if self.telegramBaseUrl in word and word != (self.telegramBaseUrl + self.telegramBaseGroupName):
                 telegramGroups.append(word.replace(self.telegramBaseUrl, ""))
 
+        # Remove duplicates
+        telegramGroups = list(dict.fromkeys(telegramGroups))
+
         printInfo(telegramGroups)
+
         return telegramGroups
-    
+
+
+    # ----------------------------------------------------------------------------------------
+
+    # Main function of this class, we only need to call this one to execute the full code
+    # What this code does is get all telegram groups from one group message and then access all the others to get all crypto contracts
     def core(self):
         upcomingCryptos = self.getUpcomingCryptos()
-        contracts = self.getUpcomingCryptos(groupNamesList=upcomingCryptos)
+        self.getUpcomingCryptos(groupNamesList=upcomingCryptos)
 
-telegram()
+
+telegram(
+    upcomingCryptosCsv="upcomingCryptos.csv"
+)
